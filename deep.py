e3fc0047957384d82b61b878368fc90f233555d8
Matthew Williams <willi367@cougars.csusm.edu>
2019-12-05 20:43:45 -0800

    Changed the code to load our custom data into the dataset
    ERROR: The tensor sizes do not match.
    
    Todo: Fix the tensor sizes.
    Todo: Read the ground truths from the CSV file.

--- deep.py
+++ deep.py
@@ -54,15 +54,22 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+data_path = './2017-IWT4S-HDR_LP-dataset/'
+trainset = torchvision.datasets.ImageFolder(
+	root=data_path,
+	transform=transform
+)
+trainloader = torch.utils.data.DataLoader(
+	trainset,
+	batch_size=64,
+	num_workers=0,
+	shuffle=True
+)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+#trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+#                                        download=True, transform=transform)
+#trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+#                                          shuffle=True, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
